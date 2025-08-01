d6d11a5a01b393d34c18c83bd186f6101f7950a6
Sasha <somelun@gmail.com>
2025-08-02 00:02:19 +0100

    Render triangle

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,32 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_opengl.h>
 
+const char* vertexShaderSource = R"(
+  #version 330 core
+  layout (location = 0) in vec3 aPos;
+  void main() {
+    gl_Position = vec4(aPos, 1.0);
+  }
+)";
+
+const char* fragmentShaderSource = R"(
+  #version 330 core
+  out vec4 FragColor;
+  void main() {
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  }
+)";
+
 struct Vertex {
   float position[3];
 };
+
+bool test_fbx(std::vector<Vertex>& out_vertices) {
+  out_vertices.push_back(Vertex{-0.5f, -0.5f, 0.0f});
+  out_vertices.push_back(Vertex{ 0.5f, -0.5f, 0.0f});
+  out_vertices.push_back(Vertex{ 0.0f,  0.5f, 0.0f});
+  return true;
+}
 
 bool load_fbx(const char* path, std::vector<Vertex>& out_vertices) {
   ufbx_load_opts opts = {0};
@@ -16,7 +39,7 @@ bool load_fbx(const char* path, std::vector<Vertex>& out_vertices) {
 
   if (!scene) {
     fprintf(stderr, "Failed to load: %s\n", error.description.data);
-    return 1;
+    return false;
   }
 
   for (size_t i = 0; i < scene->meshes.count; i++) {
@@ -43,8 +66,8 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
@@ -75,15 +98,32 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  std::vector<Vertex> vertices;
   if (argc < 2) {
-    std::cerr << "No input parameters" << std::endl;
-    return 1;
+    if (!test_fbx(vertices)) {
+      return 1;
+    }
+  } else {
+    if (!load_fbx(argv[1], vertices)) {
+      return 1;
+    }
   }
 
-  std::vector<Vertex> vertices;
-  if (!load_fbx(argv[1], vertices)) {
-    return 1;
-  }
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+  glCompileShader(vertexShader);
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+  glCompileShader(fragmentShader);
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
   GLuint vao, vbo;
   glGenVertexArrays(1, &vao);
@@ -92,8 +132,13 @@ int main(int argc, char* argv[]) {
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), (void*)0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  glViewport(0, 0, 800, 600);
 
   bool running = true;
   SDL_Event event;
@@ -115,14 +160,17 @@ int main(int argc, char* argv[]) {
     glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    glUseProgram(shaderProgram);
     glBindVertexArray(vao);
-    glDrawArrays(GL_POINTS, 0, (GLsizei)vertices.size());
+
+    glDrawArrays(GL_TRIANGLES, 0, (GLsizei)vertices.size());
 
     SDL_GL_SwapWindow(window);
   }
 
-  glDeleteBuffers(1, &vbo);
   glDeleteVertexArrays(1, &vao);
+  glDeleteBuffers(1, &vbo);
+  glDeleteProgram(shaderProgram);
 
   SDL_GL_DestroyContext(context);
   SDL_DestroyWindow(window);
