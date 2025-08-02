017d4801a2dde6611d2b5c6c4ce09a936156318b
Sasha <somelun@gmail.com>
2025-08-02 16:43:17 +0100

    Add colors to triangles

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,24 @@ constexpr int Height = 600;
 const char* vertexShaderSource = R"(
   #version 330 core
   layout (location = 0) in vec3 aPos;
+  layout (location = 1) in vec3 aColor;
+
+  out vec3 color;
+
   void main() {
     gl_Position = vec4(aPos, 1.0);
+    color = aColor;
   }
 )";
 
 const char* fragmentShaderSource = R"(
   #version 330 core
   out vec4 FragColor;
+
+  in vec3 color;
+
   void main() {
-    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    FragColor = vec4(color, 1.0);
   }
 )";
 
@@ -30,12 +38,13 @@ struct Vertex {
 
 constexpr float s = 1.73205080757f;// sqrtf(3.0f);
 GLfloat vertices[] = {
-  -0.5f,       -0.5f * s / 3.0f,        0.0f,
-   0.5f,       -0.5f * s / 3.0f,        0.0f,
-   0.0f,        0.5f * s * 2.0f / 3.0f, 0.0f,
-  -0.5f / 2.0f, 0.5f * s / 6.0f,        0.0f,
-   0.5f / 2.0f, 0.5f * s / 6.0f,        0.0f,
-   0.0f,       -0.5f * s / 3.0f,        0.0f
+// position                                 | color
+  -0.5f,       -0.5f * s / 3.0f,        0.0f, 0.8f, 0.3f,  0.02f, 
+   0.5f,       -0.5f * s / 3.0f,        0.0f, 0.8f, 0.3f,  0.02f,
+   0.0f,        0.5f * s * 2.0f / 3.0f, 0.0f, 1.0f, 0.6f,  0.32f,
+  -0.5f / 2.0f, 0.5f * s / 6.0f,        0.0f, 0.9f, 0.45f, 0.17f,
+   0.5f / 2.0f, 0.5f * s / 6.0f,        0.0f, 0.9f, 0.45f, 0.17f,
+   0.0f,       -0.5f * s / 3.0f,        0.0f, 0.8f, 0.3f,  0.02f
 };
 
 GLuint indices[] = {
@@ -152,8 +161,10 @@ int main(int argc, char* argv[]) {
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
